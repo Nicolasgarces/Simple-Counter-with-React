e039f00641285a05104d7f1620bfe8eba41023b9
Nicolas Garces <nicolasgarces0@gmail.com>
2022-01-26 21:35:47 +0000

    se agregó la actividad pero falta definir algunos detalles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Card";
+import PropTypes from "prop-types";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -7,32 +7,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 	console.log(props);
 	return (
-		<div>
-			<div className="container">
-				<div className="row">
-					<div className="col">{props.contador}</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
-					<div className="col">
-						<Card />
-					</div>
+		<div className="container">
+			<div className="row h">
+				<div className="col digit">
+					<i className="far fa-clock reloj"></i>
 				</div>
+				<div className="col digit">{props.contador6}</div>
+				<div className="col digit">{props.contador5}</div>
+				<div className="col digit">{props.contador4}</div>
+				<div className="col digit">{props.contador3}</div>
+				<div className="col digit">{props.contador2}</div>
+				<div className="col digit">{props.contador1}</div>
 			</div>
 		</div>
 	);
 };
 
+Home.propTypes = {
+	contador1: PropTypes.string,
+	contador2: PropTypes.string,
+	contador3: PropTypes.string,
+	contador4: PropTypes.string,
+	contador5: PropTypes.string,
+	contador6: PropTypes.string,
+};
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,46 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-let contador = 0;
+let contador1 = 0;
+let contador2 = 0;
+let contador3 = 0;
+let contador4 = 0;
+let contador5 = 0;
+let contador6 = 0;
 setInterval(() => {
-	contador++;
+	contador1++;
+	if (contador1 > 3) {
+		contador2++;
+		contador1 = 0;
+		if (contador2 > 3) {
+			contador3++;
+			contador2 = 0;
+			if (contador3 > 3) {
+				contador4++;
+				contador3 = 0;
+				if (contador4 > 3) {
+					contador5++;
+					contador4 = 0;
+					if (contador5 > 3) {
+						contador6++;
+						contador5 = 0;
+						if (contador6 > 3) {
+							contador6 = 1;
+						}
+					}
+				}
+			}
+		}
+	}
 	ReactDOM.render(
-		<Home contador={contador} />,
+		<Home
+			contador1={contador1}
+			contador2={contador2}
+			contador3={contador3}
+			contador4={contador4}
+			contador5={contador5}
+			contador6={contador6}
+		/>,
 		document.querySelector("#app")
 	);
 }, 1000);
